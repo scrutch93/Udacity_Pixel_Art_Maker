a19ac728d446c57ef20242957390b99af5685105
Steven <stevccru@icloud.com>
2022-03-28 13:33:07 -0400

    added variables for color picking and dimension selecting

--- designs.js
+++ designs.js
@@ -1,10 +1,21 @@
 // Select color input
+
+var colorPick = getElementbyId(colorPicker);
+
 // Select size input
+
+var gridHeight = getElementById(inputHeight);
+
+var gridWidth = getElementById(inputWidth);
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
 // Your code goes here!
+
+
+
+
 
 }
