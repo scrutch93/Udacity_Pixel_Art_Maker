701f8bc85ca445d24e5cfff8ae3a18befadaf1b9
Steven <stevccru@icloud.com>
2022-04-09 12:55:57 -0400

    updated

--- designs.js
+++ designs.js
@@ -6,22 +6,26 @@
 
 
 
+
+var canvasSize = document.getElementById("pixelCanvas");
+
+
 // When size is submitted by the user, call makeGrid()
 
 canvasSize.addEventListener("button", function(){
-    var gridHeight = document.getElementById('inputHeight').value;// get height
+    var gridHeight = document.getElementById("inputHeight").value;// get height
 
-    var gridWidth = document.getElementById('inputWidth').value;// get width
+    var gridWidth = document.getElementById("inputWidth").value;// get width
 
 
-    alert(gridHeight + " " + gridWidth);
+    console.log(gridHeight + " " + gridWidth);
 
 
    document.getElementById("pixelCanvas").innerHTML = "";//clearing out the table
 
 
 
-    makeGrid(height, width);// call the makeGrid function and pass it the height and width values.
+    makeGrid(gridHeight, gridWidth);// call the makeGrid function and pass it the height and width values.
 
 });
 
