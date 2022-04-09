a2488b3c04b80e34c97e98b33fae55fb5ebc0a0f
Steven <stevccru@icloud.com>
2022-04-08 22:20:58 -0400

    updated

--- designs.js
+++ designs.js
@@ -1,18 +1,38 @@
 // Select color input
 
-var colorPick = getElementbyId(colorPicker);
+//var colorPick = document.getElementById('colorPicker');
 
 // Select size input
 
-var gridHeight = getElementById(inputHeight);
 
-var gridWidth = getElementById(inputWidth);
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+canvasSize.addEventListener("button", function(){
+    var gridHeight = document.getElementById('inputHeight').value;// get height
+
+    var gridWidth = document.getElementById('inputWidth').value;// get width
+
+
+    alert(gridHeight + " " + gridWidth);
+
+
+   document.getElementById("pixelCanvas").innerHTML = "";//clearing out the table
+
+
+
+    makeGrid(height, width);// call the makeGrid function and pass it the height and width values.
+
+});
+
+
+function makeGrid(h, w) {
 
 // Your code goes here!
+    
+
+    
+
 
 
 
