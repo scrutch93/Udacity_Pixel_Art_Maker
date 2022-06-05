a4f3d0fd44962462665be2d78a022770a67c9eca
Steven <stevccru@icloud.com>
2022-06-05 15:00:17 -0400

    adding in descriptive comments; primarily for function

--- designs.js
+++ designs.js
@@ -1,5 +1,7 @@
+//Here we are defining the variable for the canvas size
 let canvasSize = document.getElementById("sizePicker");
 
+//Below we are taking the canvasSize variable and creating an event listener to allow the user to submit the needed criteria to set up the grid.
 canvasSize.addEventListener("submit", function (event) {
   event.preventDefault();
   document.getElementById("pixelCanvas").innerHTML = "";
@@ -11,6 +13,11 @@ canvasSize.addEventListener("submit", function (event) {
   makeGrid(gridWidth, gridHeight);
 });
 
+/**
+ * @desc Below we set up a function to actually make the table grid. For this we set up a loop to cycle through each row and column and append elements to make the grid visible.
+ * @param {*} width - this param will be passing in the user's value selection on width of the grid.
+ * @param {*} height - this param will be passing in the user's value selection on height of the grid.
+ */
 function makeGrid(width, height) {
   let table = document.getElementById("pixelCanvas");
 
@@ -24,16 +31,14 @@ function makeGrid(width, height) {
     }
   }
 
- 
-
+//While still in the scope of the makeGrid() function, we create another event listener, this time for the user to target each cell in the grid and update each cell color.
   table.addEventListener("click", function (event) {
-    
+
     let color = document.getElementById("colorPicker").value;
     let cell = event.target.closest("td");
     
     cell.style.backgroundColor = color;
     
-    console.log(color);
   });
   
  
